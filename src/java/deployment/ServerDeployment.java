bea36d57a078775e35babdc5fa92d11edfdd87fb
kristian94 <kristian.o.g.nielsen@gmail.com>
2016-04-20 22:40:07 +0200

    first commit

---
+++ src/java/deployment/ServerDeployment.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deployment;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ *
+ * @author Kristian Nielsen
+ */
+public class ServerDeployment implements ServletContextListener {
+
+    public static String PU_NAME = "PU-Local";
+    public static String[] AIRLINE_URLS = {};
+    
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
