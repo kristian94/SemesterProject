b709cfc711034b3fc18f153a17d7399c480fca84
CBorum <christopherborum@gmail.com>
2016-04-28 00:58:28 +0200

    json fix + result list

--- web/js/app.js
+++ web/js/app.js
@@ -1,15 +1,25 @@
 var bonierApp = angular.module('bonierApp', ['ngRoute', 'bonierControllers'])
 bonierApp.config(function($routeProvider) {
-        $routeProvider.
-		when('/flights', {
-            templateUrl: 'partials/result-list.html',
-            controller: 'resultListCtrl'
-        }).
-		when('/start', {
-            templateUrl: 'partials/start.html',
-            controller: 'searchCtrl'
-        }).
-        otherwise({
-            redirectTo: '/start',
-        });
+    $routeProvider.
+    when('/flights', {
+        templateUrl: 'partials/result-list.html',
+        controller: 'resultListCtrl'
+    }).
+    when('/start', {
+        templateUrl: 'partials/start.html',
+        controller: 'searchCtrl'
+    }).
+    when('/register', {
+        templateUrl: 'partials/registration.html',
+        controller: ''
+    }).
+    otherwise({
+        redirectTo: '/start',
     });
+});
+bonierApp.directive('boniernavbar', function() {
+    return {
+        restrict : 'E',
+        templateUrl: 'partials/navbar.html'
+    };
+});
