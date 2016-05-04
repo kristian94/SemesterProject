a53b32a1ad52f4d074aec6afc7e2be37a9b6578d
kristian94 <kristian.o.g.nielsen@gmail.com>
2016-05-04 12:41:59 +0200

    moved some data from AppLoginCtrl to authFactory to make them more accesible

--- web/js/auth.js
+++ web/js/auth.js
@@ -1,137 +1,156 @@
 angular.module('bonierSecurity', [])
-    .controller('AppLoginCtrl', function($scope, $rootScope, $http, $window, $location, $uibModal, jwtHelper, AuthFactory) {
+        .controller('AppLoginCtrl', function ($scope, $rootScope, $http, $window, $location, $uibModal, jwtHelper, authFactory) {
 
-        $rootScope.$on('logOutEvent', function() {
-            $scope.logout();
-        });
+            $rootScope.$on('logOutEvent', function () {
+                $scope.logout();
+            });
 
-        $scope.$on("NotAuthenticatedEvent", function(event, res) {
-            $scope.$emit("logOutEvent");
+            $scope.$on("NotAuthenticatedEvent", function (event, res) {
+                $scope.$emit("logOutEvent");
 
-            if (typeof res.data.error !== "undefined" && res.data.error.message) {
-                if (res.data.error.message.indexOf("No authorization header") === 0) {
-                    //Provide a friendly message
-                    $scope.openErrorModal("You are not authenticated to perform this operation. Please login");
-                } else {
+                if (typeof res.data.error !== "undefined" && res.data.error.message) {
+                    if (res.data.error.message.indexOf("No authorization header") === 0) {
+                        //Provide a friendly message
+                        $scope.openErrorModal("You are not authenticated to perform this operation. Please login");
+                    }
+                    else {
+                        $scope.openErrorModal(res.data.error.message);
+                    }
+                }
+                else {
+                    //You should never get here - format your error messages as suggested by the seed (backend)
+                    $scope.openErrorModal("You are not authenticated");
+                }
+
+            });
+
+            $scope.$on("NotAuthorizedEvent", function (event, res) {
+                if (typeof res.data.error !== "undefined" && res.data.error.message) {
                     $scope.openErrorModal(res.data.error.message);
                 }
-            } else {
-                //You should never get here - format your error messages as suggested by the seed (backend)
-                $scope.openErrorModal("You are not authenticated");
-            }
-
-        });
-
-        $scope.$on("NotAuthorizedEvent", function(event, res) {
-            if (typeof res.data.error !== "undefined" && res.data.error.message) {
-                $scope.openErrorModal(res.data.error.message);
-            } else {
-                $scope.openErrorModal("You are not authorized to perform the requested operation");
-            }
-        });
-
-        $scope.$on("HttpErrorEvent", function(event, res) {
-            if (typeof res.data.error !== "undefined" && res.data.error.message) {
-                $scope.openErrorModal(res.data.error.message);
-            } else {
-                $scope.openErrorModal("Unknown error during http request");
-            }
-        });
-
-        clearUserDetails($scope);
-
-        $scope.login = function() {
-            $http.post('api/login', $scope.user)
-                .success(function(data) {
-                    $window.sessionStorage.id_token = data.token;
-                    initializeFromToken($scope, $window.sessionStorage.id_token, jwtHelper);
-                    $location.path("#/view1");
-                })
-                .error(function(data) {
-                    delete $window.sessionStorage.id_token;
-                    clearUserDetails($scope);
-                });
-        };
-
-        $rootScope.logout = function() {
-            $scope.isAuthenticated = false;
-            $scope.isAdmin = false;
-            $scope.isUser = false;
-            delete $window.sessionStorage.id_token;
-            $location.path("/view1");
-        };
-
-        $rootScope.openErrorModal = function(text) {
-            var modalInstance = $uibModal.open({
-                animation: true,
-                templateUrl: 'errorModal.html',
-                controller: function($scope, $uibModalInstance) {
-                    $scope.error = text;
-                    $scope.ok = function() {
-                        $uibModalInstance.close();
-                    };
-                },
-                size: 'sm'
-            });
-        };
-
-        //This sets the login data from session store if user pressed F5 (You are still logged in)
-        var init = function() {
-            var token = $window.sessionStorage.id_token;
-            if (token) {
-                initializeFromToken($scope, $window.sessionStorage.id_token, jwtHelper);
-            }
-        };
-        init(); // and fire it after definition
-        AuthFactory.setScope($scope);
-    })
-    .factory('AuthInterceptor', function($rootScope, $q) {
-        return {
-            responseError: function(response) {
-                var name = "";
-                switch (response.status) {
-                    case 401:
-                        name = "NotAuthenticatedEvent";
-                        break;
-                    case 403:
-                        name = "NotAuthorizedEvent";
-                        break;
-                    default:
-                        name = "HttpErrorEvent";
+                else {
+                    $scope.openErrorModal("You are not authorized to perform the requested operation");
                 }
-                $rootScope.$broadcast(name, response);
-                return $q.reject(response);
-            }
-        };
-    })
-    .config(function Config($httpProvider, jwtInterceptorProvider) {
-        jwtInterceptorProvider.tokenGetter = function() {
-            return sessionStorage.getItem('id_token');
-        };
-        $httpProvider.interceptors.push('jwtInterceptor');
-    });
+            });
+
+            $scope.$on("HttpErrorEvent", function (event, res) {
+                if (typeof res.data.error !== "undefined" && res.data.error.message) {
+                    $scope.openErrorModal(res.data.error.message);
+                }
+                else {
+                    $scope.openErrorModal("Unknown error during http request");
+                }
+            });
+
+            clearUserDetails($scope, authFactory);
+
+            $scope.login = function () {
+                $http.post('api/login', $scope.user)
+                        .success(function (data) {
+                            $window.sessionStorage.id_token = data.token;
+                            initializeFromToken($scope, $window.sessionStorage.id_token, jwtHelper, authFactory);
+                            $location.path("#/view1");
+                        })
+                        .error(function (data) {
+                            delete $window.sessionStorage.id_token;
+                            clearUserDetails($scope, authFactory);
+                        });
+            };
+
+            $rootScope.logout = function () {
+                $scope.isAuthenticated = false;
+                $scope.isAdmin = false;
+                $scope.isUser = false;
+                delete $window.sessionStorage.id_token;
+                $location.path("/view1");
+            };
+
+            $rootScope.openErrorModal = function (text) {
+                var modalInstance = $uibModal.open({
+                    animation: true,
+                    templateUrl: 'errorModal.html',
+                    controller: function ($scope, $uibModalInstance) {
+                        $scope.error = text;
+                        $scope.ok = function () {
+                            $uibModalInstance.close();
+                        };
+                    },
+                    size: 'sm'
+                });
+            };
+
+            //This sets the login data from session store if user pressed F5 (You are still logged in)
+            var init = function () {
+                var token = $window.sessionStorage.id_token;
+                if (token) {
+                    initializeFromToken($scope, $window.sessionStorage.id_token, jwtHelper, authFactory);
+                }
+            };
+            init();// and fire it after definition
+        })
+        .factory('AuthInterceptor', function ($rootScope, $q) {
+            return {
+                responseError: function (response) {
+                    var name = "";
+                    switch (response.status) {
+                        case 401:
+                            name = "NotAuthenticatedEvent";
+                            break;
+                        case 403:
+                            name = "NotAuthorizedEvent";
+                            break;
+                        default :
+                            name = "HttpErrorEvent";
+                    }
+                    $rootScope.$broadcast(name, response);
+                    return $q.reject(response);
+                }
+            };
+        })
+        .config(function Config($httpProvider, jwtInterceptorProvider) {
+            jwtInterceptorProvider.tokenGetter = function () {
+                return sessionStorage.getItem('id_token');
+            };
+            $httpProvider.interceptors.push('jwtInterceptor');
+        });
 
 
 
-function initializeFromToken($scope, token, jwtHelper) {
-    $scope.isAuthenticated = true;
+function initializeFromToken($scope, token, jwtHelper, authFactory) {
+//    $scope.isAuthenticated = true;
+    authFactory.setIsAuthenticated = true;
     var tokenPayload = jwtHelper.decodeToken(token);
-    $scope.username = tokenPayload.username;
-    $scope.isAdmin = false;
-    $scope.isUser = false;
-    tokenPayload.roles.forEach(function(role) {
+//    $scope.username = tokenPayload.username;
+    authFactory.setUsername = tokenPayload.username;
+//    $scope.isAdmin = false;
+    authFactory.setIsAdmin = false;
+//    $scope.isUser = false;
+    authFactory.setIsUser = false;
+    tokenPayload.roles.forEach(function (role) {
         if (role === "Admin") {
-            $scope.isAdmin = true;
+//            $scope.isAdmin = true;
+            authFactory.setIsAdmin = true;
         }
         if (role === "User") {
-            $scope.isUser = true;
+//            $scope.isUser = true;
+            authFactory.setIsUser = true;
         }
     });
 }
 
-function clearUserDetails($scope) {
-    $scope.username = "";
-    $scope.isAuthenticated = false;
-    $scope.isAdmin = false;
-    $scope.isUser = false;
+function clearUserDetails($scope, authFactory) {
+//    $scope.username = "";
+    authFactory.setUsername = "";
+//    $scope.isAuthenticated = false;
+    authFactory.setIsAuthenticated = false;
+//    $scope.isAdmin = false;
+    authFactory.setIsAdmin = false;
+//    $scope.isUser = false;
+    authFactory.setIsUser = false;
 }
+
+
+
+
+
+
--- web/js/factories.js
+++ web/js/factories.js
@@ -3,55 +3,75 @@
 /* Place your global Factory-service in this file */
 
 angular.module('bonierFactories', []).
-factory('InfoFactory', function() {
-    var info = "Hello World from a Factory";
-    var getInfo = function getInfo() {
-        return info;
-    };
-    return {
-        getInfo: getInfo
-    };
-}).factory('AuthFactory', function() {
+        factory('InfoFactory', function () {
+            var info = "Hello World from a Factory";
+            var getInfo = function getInfo() {
+                return info;
+            };
+            return {
+                getInfo: getInfo
+            };
+        }).factory('AuthFactory', function () {
 
     var $scope;
-    var isUser;
+    var isAuthenticated;
+    var username;
     var isAdmin;
+    var isUser;
 
     function getScope() {
         return $scope;
-    };
+    }
+    ;
 
     function setScope(input) {
         $scope = input;
-    };
+    }
+    ;
 
-    return {
-        getScope: getScope,
-        setScope: setScope
-    };
+    function getIsAuthenticated() {
+        return isAuthenticated;
+    }
 
-    function getIsUser() {
-        return isUser;
-    };
+    function getUsername() {
+        return username;
+    }
 
     function getIsAdmin() {
         return isAdmin;
-    };
+    }
 
-    function setIsUser(input) {
-        isUser = input;
-    };
+    function getIsUser() {
+        return isUser;
+    }
+
+    function setIsAuthenticated(input) {
+        isAuthenticated = input;
+    }
+
+    function setUsername(input) {
+        username = input;
+    }
 
     function setIsAdmin(input) {
         isAdmin = input;
-    };
+    }
+
+    function setIsUser(input) {
+        isUser = input;
+    }
 
     return {
-        getIsUser: getIsUser,
+        getScope: getScope,
+        getIsAuthenticated: getIsAuthenticated,
+        getUsername: getUsername,
         getIsAdmin: getIsAdmin,
-        setIsUser: setIsUser,
-        setIsAdmin: setIsAdmin
-
+        getIsUser: getIsUser,
+        setScope: setScope,
+        setIsAuthenticated: setIsAuthenticated,
+        setUsername: setUsername,
+        setIsAdmin: setIsAdmin,
+        setIsUser: setIsUser
     };
 
 });
