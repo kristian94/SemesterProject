57e4b2c559c601cd75d1c41295da383ae465804e
CBorum <christopherborum@gmail.com>
2016-05-08 13:28:41 +0200

    few updates to angular

--- web/js/filters.js
+++ web/js/filters.js
@@ -1,22 +1,42 @@
-
-angular.module("bonierFilters", [])
-        .filter("dateFilter", function () {
-            return function(input){
-                return input;
-            };
-            
-            
-            
-        }).filter("searchFilter", function() {
-            return function(input, params){
-                return (
-                        input.totalPrice < params.maxPrice &&
-                        input.traveltime < params.maxTime
-                        
-                        ) ? input : null;
-            };
-        });
-        
-                
-
-
+angular.module("bonierFilters", ['bonierFactories'])
+    .filter("dateFilter", function() {
+        return function(input) {
+            return input;
+        };
+    })
+    .filter("ttimeFilter", function() {
+        return function(flights, ttime) {
+            ttime = (ttime == undefined) ? 1 : ttime;
+            var filtered = [];
+            angular.forEach(flights, function(flights) {
+                if (ttime <= flights.traveltime) {
+                    filtered.push(flights);
+                }
+            });
+            return filtered;
+        };
+    })
+    .filter("priceFilter", function() {
+        return function(flights, price) {
+            if (price == undefined) return flights;
+            var filtered = [];
+            angular.forEach(flights, function(flights) {
+                if (price >= flights.totalPrice) {
+                    filtered.push(flights);
+                }
+            });
+            return filtered;
+        };
+    })
+    //works, but shouldn't actually be necessary since server should return the needed tickets if avaliable?
+    .filter("seatFilter", function() {
+        return function(flights, requestedSeats) {
+            var filtered = [];
+            angular.forEach(flights, function(flights) {
+                if (requestedSeats <= flights.numberOfSeats) {
+                    filtered.push(flights);
+                }
+            });
+            return filtered;
+        };
+    });
